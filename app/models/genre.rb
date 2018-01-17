571e918504b3ac9e81d254458751f2fa9054cd51
Brittany Morris <brittmmendez@gmail.com>
2018-01-17 15:20:53 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,6 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    self.songs.collect do |song|
+      song.artist.name
   end
 end
